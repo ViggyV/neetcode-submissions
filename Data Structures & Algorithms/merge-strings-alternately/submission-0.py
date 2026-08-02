e9d6ef2a521bcf28bcde2d693ec407380f70f4a7
Vignesh Vasan <36783496+ViggyV@users.noreply.github.com>
2026-08-02 02:13:45 -0500

    Add: merge-strings-alternately - submission-0

---
+++ Data Structures & Algorithms/merge-strings-alternately/submission-0.py
@@ -0,0 +1,14 @@
+class Solution:
+    def mergeAlternately(self, word1: str, word2: str) -> str:
+        res = ""
+        minLen = min(len(word1), len(word2))
+
+        for i in range(minLen):
+            res += word1[i] + word2[i]
+
+        if len(word1) < len(word2):
+            res += word2[minLen:]
+        elif len(word1) > len(word2):
+            res+= word1[minLen:]
+
+        return res
